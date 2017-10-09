eb422ae56bf9ee4ad72ca77f12eba8563a03a93c
anupamadhareshwar <anupamadhareshwar@gmail.com>
2017-10-09 17:09:07 +0200

    Added Tabs

---
+++ app/src/main/java/edu/chalmers/bookscanner/SecondFragment.java
@@ -0,0 +1,22 @@
+package edu.chalmers.bookscanner;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class SecondFragment extends Fragment {
+    private static final String TAG="Tab2Fragment";
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.second_fragment, container, false);
+
+        return view;
+    }
+}

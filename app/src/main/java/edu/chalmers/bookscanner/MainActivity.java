24bf9e244f0660f89ee7ac6cc06872914c413b7f
Gedlund <fredrik.g.edlund@gmail.com>
2017-10-09 13:51:53 +0200

    Testing out creating a video player for our project, this version has a play button on top of the video player allowing you to click the button to start the video (user friendlier).

--- app/src/main/java/edu/chalmers/bookscanner/MainActivity.java
+++ app/src/main/java/edu/chalmers/bookscanner/MainActivity.java
@@ -1,40 +1,63 @@
 package edu.chalmers.bookscanner;
 
+import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.media.Image;
+import android.media.ImageWriter;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.VideoView;
 import android.widget.MediaController;
 
 
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton playButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+       // playButton.setOnClickListener(new
 
         getWindow().setFormat(PixelFormat.UNKNOWN);
 
         // Displays a video file
 
-        VideoView mVideoView2 = (VideoView) findViewById(R.id.VideoView1);
+        final VideoView mVideoView2 = (VideoView) findViewById(R.id.VideoView1);
 
         String uriPath2 = "android.resource://edu.chalmers.bookscanner/"+ R.raw.gotseason2;
         Uri uri2 = Uri.parse(uriPath2);
         mVideoView2.setVideoURI(uri2);
         mVideoView2.requestFocus();
-        MediaController mediaController = new MediaController(this);
+        final MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(mVideoView2);
         mVideoView2.setMediaController(mediaController);
 
-        mVideoView2.start();
+        mVideoView2.pause();
+
+        playButton = (ImageButton) findViewById(R.id.play_button);
+        playButton.setColorFilter(Color.argb(255,255,255,255));     // Sets the color of the playbutton to what you specify
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mVideoView2.isPlaying()) {
+                    mVideoView2.pause();
+
+                } else {
+                    mVideoView2.start();
+                    mediaController.show();
+                    playButton.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
     }
 }

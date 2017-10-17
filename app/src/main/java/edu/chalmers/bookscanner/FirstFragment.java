0d6f7452e94b45751bf3a7eff0134b2ea5003ceb
Gedlund <fredrik.g.edlund@gmail.com>
2017-10-17 12:56:21 +0200

    WorkingPopup

--- app/src/main/java/edu/chalmers/bookscanner/FirstFragment.java
+++ app/src/main/java/edu/chalmers/bookscanner/FirstFragment.java
@@ -1,21 +1,18 @@
 package edu.chalmers.bookscanner;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class FirstFragment extends Fragment {
 
@@ -24,28 +21,28 @@ private static final String TAG="Tab1Fragment";
     private ImageButton cercieButton1;
     private PopupWindow popupWindow;
     private LayoutInflater layoutInflater;
-    private RelativeLayout relativeLayout;
+    private FrameLayout bookLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.first_fragment, container, false);
 
+        cercieButton1 = (ImageButton) view.findViewById(R.id.Cercie1);
+        bookLayout = (FrameLayout) view.findViewById(R.id.bookframe);
 
-        cercieButton1 = (ImageButton) view.findViewById(R.id.Cercie);
-        relativeLayout = (RelativeLayout) view.findViewById(R.id.test);
 
         cercieButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
-                layoutInflater.getContext().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                ViewGroup container = (ViewGroup)  layoutInflater.inflate(R.layout.second_fragment,null);
+                layoutInflater = (LayoutInflater) getContext().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view = (View)  layoutInflater.inflate(R.layout.test,null);
 
-                popupWindow = new PopupWindow(container,400,400,true);
-                popupWindow.showAtLocation(relativeLayout, Gravity.NO_GRAVITY,500,500);
+                popupWindow = new PopupWindow(view,400,400,true);
+                popupWindow.showAtLocation(bookLayout, Gravity.CENTER,0,0);
 
-                container.setOnTouchListener(new View.OnTouchListener() {
+                view.setOnTouchListener(new View.OnTouchListener() {
                     @Override
                     public boolean  onTouch(View view, MotionEvent motionEvent) {
                         popupWindow.dismiss();
